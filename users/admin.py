64b727a6cca965c72d775f9841576847fcdf18ab
andriyky <akrechovetskiy@gmail.com>
2025-06-07 17:29:15 +0300

    fix: RUF005 Ruff error

--- users/admin.py
+++ users/admin.py
@@ -1,39 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
 
 @admin.register(get_user_model())
-class UserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
+class UserAdmin(DefaultUserAdmin):
+    fieldsets = (("Additional info", {"fields": ("phone_number", "image")}), *DefaultUserAdmin.fieldsets)
+    add_fieldsets = (
         (
             (
                 "Additional info",
-                {
-                    "fields": (
-                        "phone_number",
-                        "image",
-                    )
-                },
-            ),
-        )
-    )  # type: ignore
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (
-            (
-                "Additional info",
-                {
-                    "fields": (
-                        "first_name",
-                        "last_name",
-                        "phone_number",
-                        "image",
-                    ),
-                },
-            ),
-        )
+                {"fields": ("first_name", "last_name", "phone_number", "image")},
+            )
+        ),
+        *DefaultUserAdmin.add_fieldsets,
     )
-    list_display = UserAdmin.list_display + (
-        "phone_number",
-        "image",
-    )  # type: ignore
+    list_display = ("phone_number", "image", *DefaultUserAdmin.list_display)
