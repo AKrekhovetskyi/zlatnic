4a650af1c41c09aca62f1271f9c43bec0274d3b4
andriyky <akrechovetskiy@gmail.com>
2025-06-07 12:57:06 +0300

    refactor: assign prod DB URL in prod

--- zlatnic/settings.py
+++ zlatnic/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -94,9 +95,10 @@ DATABASES = {
     }
 }
 
-# dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_from_env)
+if not DEBUG:
+    # dj_database_url
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
