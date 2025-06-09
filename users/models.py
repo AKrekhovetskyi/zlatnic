b4137985413f8f1b671457ebc8a5dcda2dac7f5a
andriyky <akrechovetskiy@gmail.com>
2025-06-09 09:55:03 +0300

    fix: PTH118 Ruff error

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
-import os
 import uuid
+from pathlib import Path
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -7,11 +7,11 @@ from django.utils.text import slugify
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-def user_image_file_path(instance: "User", filename: str) -> str:
-    _, extension = os.path.splitext(filename)
+def user_image_file_path(instance: "User", filename: str) -> Path:
+    extension = Path(filename).suffix
     filename = f"{slugify(instance.username)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("profile_picture/", filename)
+    return Path("profile_picture").joinpath(filename)
 
 
 class User(AbstractUser):
