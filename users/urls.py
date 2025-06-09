28bf3b1f932760a7c7e1b202eba522abd1b67cbf
andriyky <akrechovetskiy@gmail.com>
2025-06-09 18:17:40 +0300

    refactor: add URL to serve static media

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from users.views import UserUpdateView, register_request
@@ -5,6 +7,7 @@ from users.views import UserUpdateView, register_request
 urlpatterns = [
     path("register/", register_request, name="register"),
     path("account/<int:pk>/", UserUpdateView.as_view(), name="account"),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
 
 app_name = "users"
